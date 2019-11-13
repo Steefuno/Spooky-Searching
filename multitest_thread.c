881eb5014fe3d98c87fb2fe9239cc3feaaa7cffd
Steven H. Nguyen <shn27@cp.cs.rutgers.edu>
2019-11-12 21:28:12 -0500

    Setup testplan in testplan.txt

--- multitest_thread.c
+++ multitest_thread.c
@@ -1,8 +1,26 @@
 #include <pthread.h>
 #include <stdio.h>
 
+int* searchData = NULL;
+
+void* scanAt(void* args)
+{
+	pthread_exit(15);
+}
+
 int search(int* data, int value, int numThreads)
 {
-	
+	searchData = data;
+
+	pthread_t threads[] = (pthread_t*)malloc(sizeof(pthread_t)*numThreads);
+	int output = -1;
+	int* status = NULL;
+
+	int threadNum = 0;
+	while (threadNum < numThreads) {
+		pthread_create(threads+threadNum, NULL, scanAt, );
+		++threadNum;
+	}
+
 	return 0;
 } 
