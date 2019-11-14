0c10b03ccded1bf92a1adbc0f812ab02225b4c14
ritale99 <rra76@scarletmail.rutgers.edu>
2019-11-14 14:08:31 -0500

    changes to proccesses search

--- multitest_proc.c
+++ multitest_proc.c
@@ -10,6 +10,11 @@ int search(int* data, int value, int numProcesses)
 
 	int i =0;
 	int value;
+	int start;
+	int end; 
+	
+	int sizeEachArr = size/numProccesses;	
+
 	//fork all the processes
 	while (i< numProccesses){
 		i++; 
@@ -17,8 +22,34 @@ int search(int* data, int value, int numProcesses)
 	
 		//use pid
 		//figure out start and ending indices using the value of i and search
-		//also need a way to figure out indices for variable sized arrays '%' if they cannot be evenly divided
-		value = search(data, ,);
+		
+		if(size % numProccesses!=0 && i==numProccesses){
+			//array cannot be divided evenly 		
+			//split up the last portion of the array here
+			
+			start = i*sizeEachArr+1; 
+			end = start + sizeEachArr + (size - (start+sizeEachArr));
+	
+			value = search(data,start,end);
+			}
+		else{
+			if (i==1){
+				start =0;
+				end = start + sizeEachArr;  
+			}
+			
+			else{
+				start = i*sizeEachArr + 1;
+				end = start+sizeEachArr;
+				value = search(data,start,end);
+
+				}
+
+			
+
+					} 
+		
+	
 	
 		//use system macros of wait()
 		//extract lower 8 bits... to see if child found the value
